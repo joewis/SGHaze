fd659b30b81dd0bcc082f6aa0e4ef86c0c9fb8d1
Joerg Wissen <74438661+joewis@users.noreply.github.com>
2026-03-26 15:19:36 +0800

    Update haze_script.py

--- haze_script.py
+++ haze_script.py
@@ -381,7 +381,7 @@ class HTMLGenerator:
                         <html lang="en">
                         <head>
                             <meta charset="UTF-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=10.0, user-scalable=yes">
                             <title>SG Haze Tracker</title>
                             <style>
                                 :root {{
